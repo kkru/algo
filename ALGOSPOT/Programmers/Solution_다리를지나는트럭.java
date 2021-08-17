a2ccf212a42f8ff38cf9343864d281eab19ce94b
minseok.shin <alstjr7388@gmail.com>
2021-08-17 22:39:12 +0900

    주식가격, 다리를 지나는 트럭 다시풀기

--- ALGOSPOT/Programmers/Solution_다리를지나는트럭.java
+++ ALGOSPOT/Programmers/Solution_다리를지나는트럭.java
@@ -7,13 +7,13 @@ public class Solution_다리를지나는트럭 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(solution(2, 10, new int[] { 7, 4, 5, 6 }));
-		System.out.println(solution(100, 100, new int[] {10}));
-		System.out.println(solution(100, 100, new int[] {10,10,10,10,10,10,10,10,10,10}));
+		System.out.println(solution(2, 10, new int[] {7,4,5,6}));
+		System.out.println(solution(100, 100, new int[] { 10 }));
+		System.out.println(solution(100, 100, new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 }));
 
 	}
 
-	public static int solution(int bridge_length, int weight, int[] truck_weights) {
+	public static int solution1(int bridge_length, int weight, int[] truck_weights) {
 
 		int timer = 0;
 		int nextTruck = 0;
@@ -21,13 +21,13 @@ public class Solution_다리를지나는트럭 {
 
 		Queue<Integer> q = new LinkedList<Integer>();
 
-		for (int i = 0; i < bridge_length-1; i++) {
+		for (int i = 0; i < bridge_length - 1; i++) {
 			q.add(0);
 		}
 		timer++;
 
 		do {
-			
+
 			if (nextTruck < truck_weights.length && curWeight + truck_weights[nextTruck] <= weight) {
 				curWeight += truck_weights[nextTruck];
 				q.add(truck_weights[nextTruck]);
@@ -41,6 +41,36 @@ public class Solution_다리를지나는트럭 {
 			}
 			timer++;
 		} while (nextTruck < truck_weights.length || curWeight > 0);
+
+		return timer;
+	}
+
+	public static int solution(int bridge_length, int weight, int[] truck_weights) {
+		int timer = 0;
+		int totalW = 0;
+
+		Queue<Integer> bridge = new LinkedList<Integer>();
+
+		for (int i = 0; i < bridge_length; i++) {
+			bridge.add(0);
+		}
+
+		int nextTruck = 0;
+
+		while (nextTruck < truck_weights.length || totalW > 0) {
+			timer++;
+
+			totalW -= bridge.poll();
+			
+			if (nextTruck < truck_weights.length && totalW + truck_weights[nextTruck] <= weight) {
+				totalW += truck_weights[nextTruck];
+				bridge.add(truck_weights[nextTruck]);
+				nextTruck++;
+			} else {
+				bridge.add(0);
+			}
+			
+		}
 
 		return timer;
 	}
