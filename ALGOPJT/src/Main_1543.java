25e03ee14ccf8da18a507499e52a465b9a4a0349
minseok <alstjr7388@gmail.com>
2023-05-27 19:24:41 +0900

    패키지 구조 변경

---
+++ ALGOPJT/src/Main_1543.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class Main_1543 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String docs = sc.nextLine();
+        String word = sc.nextLine();
+
+        int count = 0;
+
+        int startIdx = 0;
+
+        while (true) {
+            int idx = docs.indexOf(word, startIdx);
+
+            if (idx < 0) {
+                break;
+            }
+
+            startIdx = idx + word.length();
+            count++;
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
--- ALGOPJT/src/Main_1919.java
+++ ALGOPJT/src/Main_1919.java
@@ -1,4 +1,3 @@
-package sol;
 
 import java.util.Scanner;
 
